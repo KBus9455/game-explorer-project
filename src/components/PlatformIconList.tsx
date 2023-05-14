88d5e55a16955212bcd0f6baba8727b70c41ec25
KBus9455 <Katarzyna.Bus9455@gmail.com>
2023-05-14 19:16:22 +0200

    add Platform Icon List component

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -5,37 +5,50 @@ import {
   FaLinux,
   FaWindows,
   FaAndroid,
+  FaSearch,
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md"; //materialdesign
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs"; //bootstrap
 import { Platform } from "../hooks/useGames";
-import { Row } from "react-bootstrap";
-import { IconType } from "react-icons";
-import { Tooltip } from "react-bootstrap";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+import { Container, Row } from "react-bootstrap";
+import { ReactElement } from "react";
+
 interface Props {
   platforms: Platform[];
 }
+
 const PlatformIconList = ({ platforms }: Props) => {
-  const iconMap: { [key: string]: IconType } = {
+  const iconMap: { [key: string]: ReactElement } = {
     // name: Playstation, slug: playstation
-    xbox: FaXbox,
-    playstation: FaPlaystation,
-    mac: FaApple,
-    linux: FaLinux,
-    pc: FaWindows,
-    android: FaAndroid,
-    ios: MdPhoneIphone,
-    nintendo: SiNintendo,
-    web: BsGlobe,
+    xbox: <FaXbox />,
+    playstation: <FaPlaystation />,
+    mac: <FaApple />,
+    linux: <FaLinux />,
+    pc: <FaWindows />,
+    android: <FaAndroid />,
+    ios: <MdPhoneIphone />,
+    nintendo: <SiNintendo />,
+    web: <BsGlobe />,
   };
 
   return (
-    <Row margin={"10px"}>
-      {platforms.map((platform) => (
-        <p key={platform.id}>{platform.name}</p>
-      ))}
-    </Row>
+    <Container>
+      <Row>
+        <ul className="iconList d-flex flex-row">
+          {platforms.map((platform) => {
+            const Icon = iconMap[platform.slug];
+            return (
+              <li key={platform.id} className="pe-1">
+                {Icon}
+              </li>
+            );
+          })}
+        </ul>
+      </Row>
+    </Container>
   );
 };
 
